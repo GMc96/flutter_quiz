fa80985f44cd5e174f79a51df131544dcfb0dd67
Gary McCormack <garymccormack@outlook.com>
2023-10-25 19:05:42 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz/start_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Colors.deepPurple, Colors.purple],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight),
+            ),
+            child: const StartScreen()),
+      ),
+    ),
+  );
+}
